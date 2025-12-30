f929ae2198a89a811186ad85e4fce1d766d7b3bb
fabyelias <fabyeliasmarquez@gmail.com>
2025-12-29 22:30:54 -0300

    Fix foreign key relation in UserDashboard: use professionals!doctor_id

--- src/pages/user/UserDashboard.jsx
+++ src/pages/user/UserDashboard.jsx
@@ -44,7 +44,7 @@ const UserDashboard = () => {
                 .from('consultations')
                 .select(`
                     *,
-                    doctor:doctor_id (
+                    professionals!doctor_id (
                         full_name
                     )
                 `)
